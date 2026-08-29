9a8c77e9d7318eadd84d57856d44a40bef52442f
SumitInBits <sumitkumar7033@gmail.com>
2026-08-29 19:58:58 +0530

    feat: vartahub init

---
+++ vartahub-meeting-service/src/test/java/com/sumitinbits/meeting/VartahubMeetingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumitinbits.meeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VartahubMeetingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
